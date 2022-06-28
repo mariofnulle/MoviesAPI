0684c4f5c1f2415eb7e432424b25b4a2d56358c6
Mário Ferrari <mario.ferrari@granitosistemas.com.br>
2022-06-28 15:03:56 -0300

    • Removed default controller data; • Added a initial movie controller configuration and model;

---
+++ MoviesAPI/Controllers/MovieController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MoviesAPI.Models;
+using System;
+using System.Collections.Generic;
+
+namespace MoviesAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class MovieController : ControllerBase
+    {
+        public static List<Movie> movies = new();
+
+        [HttpPost]
+        public void AddMovie([FromBody] Movie movie)
+        {
+            movies.Add(movie);
+            Console.WriteLine(movie.Title);
+        }
+
+    }
+}
---
+++ MoviesAPI/Models/Movie.cs
@@ -0,0 +1,10 @@
+﻿namespace MoviesAPI.Models
+{
+    public class Movie
+    {
+        public string Title { get; set; }
+        public string Director { get; set; }
+        public string Gender { get; set; }
+        public int Duration { get; set; }
+    }
+}
