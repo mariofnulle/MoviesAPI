242a4ec39a306bebf76999e313168b70696cf95a
Mário Ferrari <mario.ferrari@granitosistemas.com.br>
2022-08-12 14:59:29 -0300

    Initial MVC setup Created the new project and started the adaptation for MVC.

---
+++ MoviesWebView/Models/UserViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MoviesWebView.Models
+{
+    public class UserViewModel
+    {
+        [Required]
+        public string UserName { get; set; }
+        [Required]
+        public string Email { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+        [Required]
+        [Compare("Password")]
+        public string RePassword { get; set; }
+    }
+}
